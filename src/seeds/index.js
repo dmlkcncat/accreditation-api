a2dc0ca05e92c7af5528ae739e2f3ac91c4cf4a6
eminakcil <eminakcil@outlook.com>
2022-12-23 16:51:09 +0300

    acc seed

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,8 +1,14 @@
 import user from './user'
 import userRole from './userRole'
+import acc from './acc'
 
 export default async () => {
+  console.log('tohumluyorum')
   await userRole()
 
   await user()
+
+  await acc()
+
+  console.log('tohumladım')
 }
