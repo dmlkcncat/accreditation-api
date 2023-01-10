b5b6f802a2e01955bbcd0a7c4f58b26a3d69137e
eminakcil <eminakcil@outlook.com>
2023-01-10 11:23:36 +0300

    participant list member - user

--- src/validations/ParticipantList.js
+++ src/validations/ParticipantList.js
@@ -1,5 +1,11 @@
 import Joi from 'joi'
 
+const participant = Joi.object({
+  type: Joi.string().valid('user', 'member').required(),
+  participant: Joi.string().required(),
+}).required()
+
 export const createValidation = Joi.object({
   businessPlan: Joi.string().required(),
-})
+  participantList: Joi.array().items(participant).required(),
+}).required()
